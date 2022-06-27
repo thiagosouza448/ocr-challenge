2914e73013fec62b1373380cd29dafc894c7db2e
Thiago Souza <31426826+thiagosouza448@users.noreply.github.com>
2022-06-27 16:12:24 -0300

    Update call.py

--- call.py
+++ call.py
@@ -1,16 +1,20 @@
 from findKey import Receipts
 from Services.OCR.OCRService import OCRService
 
-ocr = OCRService(r"C:\AUT_RPA_V2\Services\Utilities\Engine\Tesseract-OCR\tesseract.exe")
 
 
-list_image = ['2022-06-08-11-55-00.jpg','2022-06-08-11-55-26.jpg', '2022-06-08-12-09-17.jpg', '2022-06-08-12-09-21.jpg']
+
+ocr = OCRService(r"OCR\Tesseract-OCR\tesseract.exe")
+# LIST IMAGE MOCK THAT GOING RECEIV
+list_image = ['2022-06-27-10-20-46.jpg','2022-06-27-10-20-58.jpg', '2022-06-27-10-21-01.jpg', '2022-06-27-10-21-13.jpg']
+path_directory = r"D:\ESTUDO\PYTHON\OCR\ocr-challenge\receipts\CV\CV645"
+# MOCK KWORDS
 key = 'AUTE'
-
-find = Receipts()
-keys = find.find_key(list_image, ocr,  key)
-key_more_freq = find.most_frequent(keys)
-
+# INSTANCE CLASS
+find = ReceiptsUtils()
+# FIND KEY
+keys = find.find_key(path_directory, list_image, ocr,  key)
 print(keys)
-print(key_more_freq)
-
+# FIND KEY MORE FREQUENT IN LIST
+key_more_frequent = find.most_frequent(keys)
+print(key_more_frequent)
